5a82d863ecc024235a5568dddf59c0490480f07c
edgorrell <edgorrell@stu.naperville203.org>
2022-10-31 14:58:53 -0500

    ee

--- pow_of_2_test/PowOfTwo.java
+++ pow_of_2_test/PowOfTwo.java
@@ -3,19 +3,22 @@ import java.util.Scanner;
 public class PowOfTwo{
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
-        int size;
+        int size, max;
         char out;
         
         System.out.println("Enter size: ");
         size = scan.nextInt();
+        System.out.println("Enter max power: ");
+        max = scan.nextInt();
         
+        System.out.print("\u000c");
         for(int i = 1; i <= size; i++){
             out = 'A';
             for(int j = 0; j < (size + "").length() - (i + "").length(); j++){
                 System.out.print(" ");
             }
             System.out.print(i + ": ");
-            for(int j = 1; (int)Math.pow(2,j) < size; j++){
+            for(int j = 1; j < size; j++){
                 if(i % (int)Math.pow(2,j) == 0){
                     System.out.print("-");
                     out++;
