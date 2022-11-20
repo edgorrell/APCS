13b2b7a4af6763afc11ab20dd9c869df67b269cc
edgorrell <edgorrell@stu.naperville203.org>
2022-11-20 14:52:12 -0600

    er

--- pow_of_2_test/PowOfTwo.java
+++ pow_of_2_test/PowOfTwo.java
@@ -11,10 +11,10 @@ public class PowOfTwo{
         
         for(int i = 1; i <= size; i++){
             out = 'A';
-            for(int j = 0; j < (size + "").length() - (i + "").length(); j++){
-                System.out.print(" ");
-            }
-            System.out.print(i + ": ");
+            //for(int j = 0; j < (size + "").length() - (i + "").length(); j++){
+            //    System.out.print(" ");
+            //}
+            //System.out.print(i + ": ");
             for(int j = 1; (int)Math.pow(2,j) < size; j++){
                 if(i % (int)Math.pow(2,j) == 0){
                     System.out.print("-");
