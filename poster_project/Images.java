d5f0e9ad11e492aa81ab28b9e8c2083dbd6990be
Evan Gorrell <111461168+edgorrell@users.noreply.github.com>
2023-05-09 10:34:30 -0500

    temple mirror hopefully

--- poster_project/Images.java
+++ poster_project/Images.java
@@ -11,4 +11,13 @@ public class Images{
     public static BufferedImage getImage(BufferedImage img, int num){
         return null;
     }
+    public static void Temple(){
+        BufferedImage temple = ImageIO.read(new File("poster_project/images/temple.png"));
+        BufferedImage sub = temple.getSubImage(15,30,260,60);
+        //(16,87),(276,29) so its (16,29) with w = 260 and h = 60
+        sub = Poster.scale(sub,-1,1);
+        Graphics2D canvas = temple.createGraphics();
+        canvas.drawImage(sub,15,30,null);
+        ImageIO.write(temple,"png",new File("poster_project/images/temple_fixed.png"));
+    }
 }
